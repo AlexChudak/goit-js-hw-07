4e6bafe134ddf2c2ad809c4c48f6dbb2a5726372
Aleksandr Chudak <63600579+AlexChudak@users.noreply.github.com>
2021-02-09 21:13:48 +0200

    Commit next

--- js/task-5.js
+++ js/task-5.js
@@ -2,29 +2,12 @@
 //(событие input), подставляет его текущее значение в span#name - output.
 //Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
-const refs = {
-  input: document.querySelector('#name-input'),
-  output: document.querySelector('#name-output'),
-};
+const inputForm = document.querySelector('input');
+const spanString = document.querySelector('#name-output');
 
-refs.input.addEventListener('input', onInputChange);
+const changeSpan = () =>
+  inputForm.value !== ''
+    ? (spanString.textContent = inputForm.value)
+    : (spanString.textContent = 'незнакомец');
 
-function onInputChange(event) {
-  refs.output.textContent !== ''
-    ? (refs.output.textContent = event.currentTarget.value)
-    : (refs.output.textContent = 'незнакомец');
-}
-
-//Через remove снять слушателя и вернуть "незнакомец"
-//  console.log(event.currentTarget.value);
-//  refs.output.textContent = event.currentTarget.value;
-
-// const inputForm = document.querySelector('input');
-// const spanString = document.querySelector('#name-output');
-
-// const changeSpan = () =>
-//   inputForm.value !== ''
-//     ? (spanString.textContent = inputForm.value)
-//     : (spanString.textContent = 'незнакомец');
-
-// inputForm.addEventListener('input', changeSpan);
+inputForm.addEventListener('input', changeSpan);
